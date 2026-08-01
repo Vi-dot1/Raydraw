3e47417201d32751d3145f8311ed9e2fedbfc0cc
Vi-dot1 <andrenieves45@gmail.com>
2026-08-01 13:49:10 -0400

    Started refactoring of the UI, mostly in a attemp of making it into something less hardcoded, a new mock up for the UI and the idea for the new system are saved on `UI.drawio` in diagrams

--- src/Gui/gui.hpp
+++ src/Gui/gui.hpp
@@ -1,19 +1,39 @@
 #pragma once
 
+#include "Utils/mouseState.hpp"
+#include "raymath.h"
 extern "C"{
     #include"raylib.h"
 }
 
-
 namespace Gui {
 
-// Used to check whenever a point is inside the main panel area
-bool IsOverPanel(const Vector2&);
+struct Component {
+    float x, y, w, h;
+
+    virtual void draw();
+};
+
+struct Panel {
+    Rectangle space;
+    float marging=0.f;
+    bool visible=true;
+
+    // Translates a position with the Panel top-right corner as its origin
+    Vector2 translatePos(const Vector2& pos) {
+        return Vector2Add(Vector2(space.x, space.y), pos);
+    }
+    bool isMouseOver() {
+        return CheckCollisionPointRec(Mouse::getMouseState().pos, space);
+    }
+};
 
 // Draw/process call for the main panel
 void draw(); 
 
 // Recalculates panels size when window is resized
+// Used to check whenever a point is inside the main panel area
+bool IsMouseOverPanel();
 void updatePanel();
 
 }
